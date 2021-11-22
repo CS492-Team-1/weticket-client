4d847998a25b8cc3c8ac2a17d3de826f6537e9b2
ramel0915 <sdy992000@kaist.ac.kr>
2021-11-22 23:26:16 +0900

    [로그인] 로그인 페이지 마크업 작성 완료 (#22)
    * PR테스트
    
    * 불필요한 파일 삭제
    
    * 로그인 페이지 디자인
    
    * styled component 이름 수정 및 static value로 수정
    
    * br대신 margin을 사용하여 마크업

--- src/pages/Login/LoginPresenter.tsx
+++ src/pages/Login/LoginPresenter.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
+import { colors } from '../../assets/styles/colors'
+import { Login } from '.';
 
 //TODO : prop type 정의
 type LoginPresenterProps = {
@@ -8,15 +11,132 @@ type LoginPresenterProps = {
 
 export const LoginPresenter: React.FC<LoginPresenterProps> = ({ login }) => {
   //TODO : react-hook-form 사용하여 로그인, 회원가입 구현
-  //https://react-hook-form.com/get-started#TypeScript
+  //
   return (
     <SContainer>
-      Login
+      <Title>WeTicket</Title>
+      <LogInContainer>
+        <SubTitle>LogIn</SubTitle>
+        <HorizonLine/>
+        <LoginForm>
+          <IDBox></IDBox>
+          <PwBox></PwBox>
+          <LoginButton>로그인</LoginButton>
+          <Messages>계정이 없으신가요?</Messages>
+          <RegisterButton>회원가입</RegisterButton>
+        </LoginForm>
+      </LogInContainer>
       <button onClick={login}>로그인</button>
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: ${colors.primary};
+  a{
+    display: content;
+  }
+  height: 100vh;
+  width : 100vw;
+
 `;
+
+const Title = styled.h1`
+  display: flex;
+  color: ${colors.white};
+  font-size: 30px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const SubTitle = styled.h2`
+  display: flex;
+  color: ${colors.black};
+  font-size: 18px;
+  font-weight: 500;
+  margin-top: 30px;
+  margin-bottom: 10px;
+`;
+
+
+const LogInContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  align-items: center;
+  width: 280px;
+  height: 350px;
+  border-radius: 15px;
+  background-color: ${colors.white};
+`;
+
+const HorizonLine = styled.hr`
+  width: 220px;
+  display: flex;
+  border-top: 1px solid ${colors.black};
+  margin-bottom: 20px;
+`;
+
+const LoginForm = styled.form`
+  border-radius: 15px;
+  border-color: none;
+  border-style: none;
+  width: 230px;
+`;
+
+const Button = styled.button`
+  font-weight: 700;
+  border-radius: 15px;
+  border-style: none;
+  width: 230px;
+  height: 30px;
+  margin: 6px;
+`;
+
+const LoginButton = styled(Button)`
+  border-color: none;
+  color: ${colors.white};
+  box-shadow: none;
+  background-color: ${colors.primary};
+  margin-bottom :20px;
+`;
+
+const RegisterButton = styled(Button)`
+  color: ${colors.black};
+  border-color: ${colors.black};
+  border-style: solid;
+  border-width: thin;
+  box-shadow: none;
+  background-color: ${colors.white};
+`;
+
+const Messages = styled.span`
+  color: ${colors.black};
+  font-size: 15px;
+  font-weight: 500;
+  margin: 6px;
+`;
+
+
+const InputBox = styled.input`
+  border-radius: 15px;
+  border-color: none;
+  border-style: none;
+  width: 230px;
+  text-align: center;
+  height: 30px;
+  margin: 3px;
+  border-style: solid;
+  border-width: thin;
+`;
+
+
+const IDBox = styled(InputBox).attrs({placeholder: "ID"})``;
+const PwBox = styled(InputBox).attrs({placeholder: "password", type:"password"})``;
+
